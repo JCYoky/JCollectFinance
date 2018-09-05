aff7e2cf7e8f060fd4dff15256b337004d336dc5
JCYoky <616663713@qq.com>
2018-09-05 23:55:14 +0800

    重构

--- src/main/java/me/hjc/updatedividends/controller/UpdateData.java
+++ src/main/java/me/hjc/updatedividends/controller/UpdateData.java
@@ -1,8 +1,6 @@
 package me.hjc.updatedividends.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import me.hjc.updatedividends.config.MappingConfig;
-import me.hjc.updatedividends.service.DividendServiceImpl;
 import me.hjc.updatedividends.service.IDividendService;
 import me.hjc.updatedividends.service.IStockService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
 
@@ -25,23 +22,21 @@ public class UpdateData implements CommandLineRunner {
     IDividendService dividendService;
 
     @Override
-    public void run(String... args) {
-        Map<String, String> stocksMap = null;
-        try {
-            stocksMap = stockService.getStocksMap();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void run(String... args) throws IOException {
+        Map<String, String> stocksMap;
+        stocksMap = stockService.getStocksMap();
         if (Objects.requireNonNull(stocksMap).size() == 0) {
             log.error("获取股票列表数据异常");
             return;
         }
-        stocksMap.forEach((key, value) -> {
-            try {
-                dividendService.upsert(key, value);
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        stocksMap.forEach(this::upsertDividend);
+    }
+
+    private void upsertDividend(String key, String value) {
+        try {
+            dividendService.upsert(key, value);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
